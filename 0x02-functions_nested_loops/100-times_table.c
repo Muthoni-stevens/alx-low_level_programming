087e89659ffabf9bacadbbd03a7a5c5c3e2e6c86
Muthoni-stevens <muthonykserah@gmail.com>
2022-09-15 04:43:04 -0700

    10 x alphabet

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+*print_times_table - prints the  times table with
+*parameter
+*@n: parameter
+*Return: returns nothing
+*/
+void print_times_table(int n)
+{
+int digit, mult, result;
+if (n <= 15 && n >= 0)
+{
+for (digit = 0; digit <= n; digit++)
+{
+_putchar('0');
+
+for (mult = 1; mult <= n; mult++)
+{
+_putchar(',');
+_putchar(' ');
+result = digit * mult;
+if (result <= 99)if (result <= 99)
+
