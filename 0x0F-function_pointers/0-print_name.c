c92949c62460a53a2f09ec47159e85ecf5b76a46
Muthoni-stevens <muthonykserah@gmail.com>
2022-10-13 13:46:40 -0700

    1. If you spend too much time thinking about a thing, you'll never get it done

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,13 +1,13 @@
 #include "function_pointers.h"
 
 /**
-* print_name -print a name
+* print_name -prints a name
 * @name:name to print
 *@f: function which print name
 */
-
- void print_name(char *name, void (*f)(char *));
+void print_name(char *name, void (*f)(char *));
 {
+
 	if (name ==NULL || f == NULL)
 		return;
 
--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -1,21 +1,29 @@
-#include "function_pointers.h"
+#include <stdarg.h>
+#include <stdio.h>
+#include "variadic_functions.h"
 
 /**
-* array_iterator - executes a function given as a
-* parameter on each element of an array.
-* @array: input integer array.
-* @size: size of the array.
-* @action: pointer to the function
-* Return: no return.
-*/
-
-void array_iterator(int *array, size_t size, void (*action)(int));
+ * print_numbers - prints numbers, followed by a new line.
+ * @separator: separator to print between numbers
+ * @n: number of numbers to print
+ *
+ * Return: void
+ */
+void print_numbers(const char *separator, const unsigned int n, ...)
 {
-	unsigned int i;	
+	unsigned int i;
+	int num;
+	va_list valist;
 
- 	if (array && action)
-
-		for (i = 0; i < size; i++)
-			action(array[i]);
+	va_start(valist, n);
+	for (i = 0; i < n; i++)
+	{
+		num = va_arg(valist, int);
+		printf("%d", num);
+		if (i < n - 1 && separator)
+			printf("%s", separator);
+	}
+	printf("\n");
+	va_end(valist);
 }
 
